54e7f6f2441262786658f5a85e65ab7614910cec
Udit Karode <udit.karode@gmail.com>
2022-09-11 20:21:08 +0530

    feat: pass forward exec nodes

--- src/generator/generator.ts
+++ src/generator/generator.ts
@@ -1,21 +1,33 @@
-import { Arrangement } from "../evaluator/utils.ts";
+import { Arrangement, Exec } from "../evaluator/utils.ts";
 import { Flow, IndividualNode, LayoutNode } from "../types.ts";
 
 export interface Layout {
 	workspace: number;
 	node: LayoutNode;
+	execNodes: Exec[];
 }
 
-function getNode(arrangement: Arrangement, percent: number): LayoutNode {
-	return {
+function getNodeAndExecNodes(
+	arrangement: Arrangement,
+	percent: number,
+): { node: LayoutNode; execNodes: Exec[] } {
+	const execNodes: Exec[] = [];
+
+	const node: LayoutNode = {
 		layout: arrangement.split == "vertical" ? "splitv" : "splith",
 		percent: percent / 100,
 		type: "con",
 		nodes: arrangement.nodes.map((node, i) => {
 			// if this node is a layout
-			if ("split" in node) return getNode(node, arrangement.ratio[i]);
+			if ("split" in node) {
+				const { node: nestedNode, execNodes: nestedExecNodes } =
+					getNodeAndExecNodes(node, arrangement.ratio[i]);
+				execNodes.push(...nestedExecNodes);
+				return nestedNode;
+			}
 			// if this node is an Exec/Individual node
-			else
+			else {
+				execNodes.push(node);
 				return {
 					name: node.programName,
 					type: "con",
@@ -26,7 +38,13 @@ function getNode(arrangement: Arrangement, percent: number): LayoutNode {
 						},
 					],
 				} as IndividualNode;
+			}
 		}),
+	};
+
+	return {
+		node,
+		execNodes,
 	};
 }
 
@@ -34,10 +52,13 @@ export const generator = (flow: Flow): Layout[] => {
 	const nodes: Layout[] = [];
 
 	for (const definition of flow.definitions) {
+		// root node always covers the screen
+		const { node, execNodes } = getNodeAndExecNodes(definition.node, 100);
+
 		nodes.push({
 			workspace: definition.workspace,
-			// root node always covers the screen
-			node: getNode(definition.node, 100),
+			node,
+			execNodes,
 		});
 	}
 
