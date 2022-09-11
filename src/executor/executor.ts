8f729ddac6530ca2ea2e44e7fae9c40428c7bddc
Udit Karode <udit.karode@gmail.com>
2022-09-11 20:11:44 +0530

    feat: listen for window changes

--- src/executor/executor.ts
+++ src/executor/executor.ts
@@ -1,14 +1,24 @@
 import { i3 } from "../deps.ts";
 import { Layout } from "../generator/generator.ts";
+import { exit } from "../utils.ts";
 
-const LAYOUT_FILE = "/tmp/zumen.ndjson";
+const LAYOUT_FILE = "/tmp/zumen.json";
 
 export const executor = async (layout: Layout) => {
 	const wm = await i3.Connect();
 
 	await Deno.writeTextFile(LAYOUT_FILE, JSON.stringify(layout.node));
 
-	await wm.runCommand(
+	const [{ success }] = await wm.runCommand(
 		`workspace ${layout.workspace}; append_layout ${LAYOUT_FILE}`,
 	);
+
+	if (!success) exit(`Unable to load the layout file at '${LAYOUT_FILE}'`);
+
+	await wm.subscribe(["WINDOW"]);
+	wm.on(i3.Events.WINDOW, ctx => {
+		if (ctx.change == "new") {
+			// check if a program we want opened
+		}
+	});
 };
