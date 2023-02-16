b3931e73f9d63313829985e49332bb824646a0e6
Udit Karode <udit.karode@gmail.com>
2023-02-17 03:31:46 +0530

    feat: use regexes in the example lisp

--- test.lisp
+++ test.lisp
@@ -1,11 +1,11 @@
 (flow "main"
   (ws 8 :pre "exec bash -c 'echo doing'" :post "exec bash -c 'echo done'"
     (horizontal '(80 20)
-      (exec "exec firefox" :class "firefox")
+      (exec "exec firefox" :class "fi.*x" :name "Mozilla Fir.*x")
       (vertical '(60 40)
          (exec "exec kitty" :class "kitty")
-         (exec "exec kitty --class=floater" :class "floater"))))
+         (exec "exec kitty --class=floater" :class "flo.*er"))))
   (ws 9 :pre "exec bash -c 'echo doing again!'"
     (vertical '(30 70)
       (exec "exec firefox" :class "firefox")
-      (exec "exec kitty" :class "kitty"))))
+      (exec "exec kitty" :class "k.*ty"))))
